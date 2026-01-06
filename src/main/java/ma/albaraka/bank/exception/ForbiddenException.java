7a9f1553732574f55a7d95e633cd57c3452790d4
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:31 +0100

    Add ForbiddenException.java

---
+++ src/main/java/ma/albaraka/bank/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package ma.albaraka.bank.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
