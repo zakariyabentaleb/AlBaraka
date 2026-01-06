07ca99c5fdd0364149d0c33701d0fb0a1676d4b1
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:31 +0100

    Add BusinessRuleException.java

---
+++ src/main/java/ma/albaraka/bank/exception/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package ma.albaraka.bank.exception;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
