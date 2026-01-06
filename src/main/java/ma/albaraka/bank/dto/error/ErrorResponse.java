5b0f5a8b0589eae2b50b96bfbce42197f3f3a615
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:28 +0100

    Add ErrorResponse.java

---
+++ src/main/java/ma/albaraka/bank/dto/error/ErrorResponse.java
@@ -0,0 +1,33 @@
+package ma.albaraka.bank.dto.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Map<String, String> validationErrors;
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
