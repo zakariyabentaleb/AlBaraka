c5703ca1bac0795d1949bbfc9fcb898246af623c
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:37 +0100

    Add AlBarakaBankApplicationTests.java

---
+++ src/test/java/ma/albaraka/bank/AlBarakaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.albaraka.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class AlBarakaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
