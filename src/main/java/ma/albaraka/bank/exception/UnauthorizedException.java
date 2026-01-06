70085b8b3e217ef51202e4e8568b42f269eee79e
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:31 +0100

    Add UnauthorizedException.java

---
+++ src/main/java/ma/albaraka/bank/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package ma.albaraka.bank.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
