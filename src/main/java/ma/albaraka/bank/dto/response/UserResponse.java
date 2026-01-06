befb438d57a765809ea774634937cadf1dc036db
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:30 +0100

    Add UserResponse.java

---
+++ src/main/java/ma/albaraka/bank/dto/response/UserResponse.java
@@ -0,0 +1,22 @@
+package ma.albaraka.bank.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    private Long id;
+    private String email;
+    private String fullName;
+    private String role;
+    private Boolean active;
+    private LocalDateTime createdAt;
+}
