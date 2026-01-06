e1472a77cb10271122201b2e279b389a47531c4d
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:29 +0100

    Add CreateOperationRequest.java

---
+++ src/main/java/ma/albaraka/bank/dto/request/CreateOperationRequest.java
@@ -0,0 +1,30 @@
+package ma.albaraka.bank.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateOperationRequest {
+
+    @NotNull(message = "Operation type is required")
+    private String type;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    private Long sourceAccountId;
+
+    private Long destinationAccountId;
+
+    private String note;
+}
