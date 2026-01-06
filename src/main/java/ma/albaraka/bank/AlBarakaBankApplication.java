2c5385638ed22fb8964fa2c37ce92206d012886f
zakariya bentaleb <zakariyabentaleb2002@gmail.com>
2026-01-06 14:19:26 +0100

    Add AlBarakaBankApplication.java

---
+++ src/main/java/ma/albaraka/bank/AlBarakaBankApplication.java
@@ -0,0 +1,13 @@
+package ma.albaraka.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlBarakaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlBarakaBankApplication.class, args);
+    }
+
+}
